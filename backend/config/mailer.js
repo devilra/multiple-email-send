6183c44e9cc397f91fb1cdf2e2b75c4ffc2a3ea7
devilra <rockerraja906@gmail.com>
2025-11-30 12:55:15 +0530

    update email send multiple

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+  // host: "smtp.gmail.com",
+  // port: 587,
+  // secure: false, // TLS
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
